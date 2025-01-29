2a41bd2221f8a1e5aece98b333746f17122e29bb
somulo <mcomulosammy37@gmail.com>
2025-01-29 16:14:37 +0300

    link the login page

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ app_name = "main"
 
 urlpatterns = [
     path('', v.home, name='home'),
-    path('logout/', v.logout, name='logout')
+    path('login/', v.login_view, name='login'),  # Add the login URL pattern
+    path('logout/', v.logout, name='logout'),  # Logout URL
 ]
