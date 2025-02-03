b04c66657d8edbb9999fb65ec12d682e9069bc62
fnamayi <franklynenm@gmail.com>
2025-02-03 17:17:55 +0300

    fix: update company url

--- company/urls.py
+++ company/urls.py
@@ -5,7 +5,7 @@ from .views import company_dashboard
 app_name = 'company'
 
 urlpatterns = [
-    path('dashboard/', company_dashboard, name='dashboard'),  # Company dashboard URL
+    path('dashboard/', company_dashboard, name='company_dashboard'),  # Company dashboard URL
     path('profile/', views.company_profile, name='company_profile'),
     path('requests/', views.service_requests, name='service_requests'),
     path('services/', views.service_list, name='service_list'),
