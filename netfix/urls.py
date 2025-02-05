dec230680c6c5a79812c7a968258290229ac6906
somulo <mcomulosammy37@gmail.com>
2025-02-05 12:10:08 +0300

    chore:update netfix

--- netfix/urls.py
+++ netfix/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.urls import re_path
 from django.contrib import admin
 from django.urls import include, path
-
+# 
 from . import views as v
 
 urlpatterns = [
