d5af3b3ae89572d84506c32bacaec16844849d85
somulo <mcomulosammy37@gmail.com>
2025-02-13 14:45:14 +0300

    feat: add service request to fetch requested services

--- main/models.py
+++ main/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 # 
+class ServiceRequest(models.Model):
+    address = models.TextField()
+    hours_needed = models.PositiveIntegerField()
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    request_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20)
+    customer_id = models.BigIntegerField()
+    service_id = models.BigIntegerField()
+
+    def __str__(self):
+        return f"Request {self.id} for {self.address}"
